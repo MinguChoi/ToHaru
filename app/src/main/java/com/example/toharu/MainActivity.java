f059f482f6d0b7628298dbe0c0fb2167a898e1dd
doyeon <raisin_9@naver.com>
2021-03-15 16:31:21 +0900

    글꼴목록, 환경설정 XML

--- app/src/main/java/com/example/toharu/MainActivity.java
+++ app/src/main/java/com/example/toharu/MainActivity.java
@@ -3,14 +3,47 @@ package com.example.toharu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    // for checking github
-    ///  fdddd
+
+    ListView text_style; // ListView 안에 글꼴 option표시
+    ArrayAdapter<String> adapter;
+    ArrayList<String> listItem; //글꼴 option 정의
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.textstyle);  //textstyle 화면 보이게 test
+
+        listItem = new ArrayList<String>();
+        listItem.add("글꼴 1");
+        listItem.add("글꼴 2");
+        listItem.add("글꼴 3");
+
+        adapter =new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_single_choice,listItem);
+        text_style =findViewById(R.id.text_style); //text_style 리스트뷰 객체 가져오기
+        text_style.setChoiceMode(text_style.CHOICE_MODE_SINGLE); //글꼴 하나만 선택가능하게함
+        text_style.setAdapter(adapter);
+
+
+        text_style.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(getApplicationContext(),listItem.get(position).toString(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
