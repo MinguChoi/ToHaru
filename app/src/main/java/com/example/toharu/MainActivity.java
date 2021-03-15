fe1a0fbc188d6a5e3ddb66e8be392b9cf8891bdf
MinguChoi <32556382+MinguChoi@users.noreply.github.com>
2021-03-15 11:17:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/toharu/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.toharu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // for checking github
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
