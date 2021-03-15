fd5c905521bd1ec5714f2d10e0ceaf96ec3f6a23
YeStar-com <80377632+YeStar-com@users.noreply.github.com>
2021-03-15 12:05:17 +0900

    startLayout

--- app/src/main/java/com/example/toharu/MainActivity.java
+++ app/src/main/java/com/example/toharu/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.toharu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     // for checking github
-    // dddddddddddddddddddddddf f 
+    // dddddddddddddddddddddddf f
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(getApplicationContext(), CalendarActivity.class);
+        startActivity(intent);
     }
 }
