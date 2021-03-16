5f2c2dfaa14fae0d0f48fedebdf9c6cd21763baa
MinguChoi <32556382+MinguChoi@users.noreply.github.com>
2021-03-16 17:56:09 +0900

    working on fetchPosts

--- app/src/main/java/com/example/toharu/API_Post.java
+++ app/src/main/java/com/example/toharu/API_Post.java
@@ -4,12 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class API_Post {
     public static void writePostToDB(Post post, Activity ctx) {
@@ -30,4 +37,10 @@ public class API_Post {
         ctx.startActivity(intent);
     }
 
+    public static List<Post> fetchPosts(){
+        User currentUser = User.getInstance();
+        List<String> postsUid = currentUser.getPosts();
+        List<Post> posts = new ArrayList<Post>();
+
+    }
 }
