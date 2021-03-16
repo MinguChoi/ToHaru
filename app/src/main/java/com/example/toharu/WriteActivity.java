9c6d252153a78239ee4dd00a6b127c462fa00e80
MinguChoi <32556382+MinguChoi@users.noreply.github.com>
2021-03-16 16:56:04 +0900

    API for writePost

--- app/src/main/java/com/example/toharu/WriteActivity.java
+++ app/src/main/java/com/example/toharu/WriteActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class WriteActivity extends AppCompatActivity {
     private Button backBTN;
     private Button saveBTN;
+    private EditText diaryETXT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class WriteActivity extends AppCompatActivity {
     public void init(){
         backBTN = findViewById(R.id.backBTN);
         saveBTN = findViewById(R.id.saveBTN);
+        diaryETXT = findViewById(R.id.diary_writeETXT);
 
         backBTN.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,9 +39,11 @@ public class WriteActivity extends AppCompatActivity {
         saveBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(WriteActivity.this, CalendarActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
+                Post newPost = new Post("Happy", "2021-03-16", diaryETXT.getText().toString());
+                API_Post.writePostToDB(newPost, WriteActivity.this);
+//                Intent intent = new Intent(WriteActivity.this, CalendarActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
             }
         });
     }
