a4cd949e834fdce00c6581370976668a48ff0b82
YeStar-com <80377632+YeStar-com@users.noreply.github.com>
2021-03-17 17:46:23 +0900

    ui 개선 전

--- app/src/main/java/com/example/toharu/AdviceActivity.java
+++ app/src/main/java/com/example/toharu/AdviceActivity.java
@@ -1,17 +1,23 @@
 package com.example.toharu;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 import java.net.HttpCookie;
 
@@ -22,6 +28,4 @@ public class AdviceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advice);
     }
-
-
 }
--- app/src/main/java/com/example/toharu/Test_EnterActivity.java
+++ app/src/main/java/com/example/toharu/Test_EnterActivity.java
@@ -6,14 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class EnterActivity extends AppCompatActivity {
-    private Button sendbt;
-    private EditText editdt;
-    public String msg;
+import java.util.HashMap;
+
+// 위로의 글 db에 넣어주려고 만든 activity
+public class Test_EnterActivity extends AppCompatActivity {
+    private Button                sendbt;
+    private EditText              editdt;
+    private Spinner               spinnerMoods;
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
@@ -21,17 +25,31 @@ public class EnterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enter);
+        setContentView(R.layout.test_advice_enter);
 
         sendbt = (Button) findViewById(R.id.button);
         editdt = (EditText) findViewById(R.id.editTextTextPersonName);
+        spinnerMoods = (Spinner) findViewById(R.id.spinner);
 
         sendbt.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-                msg = editdt.getText().toString();
-                databaseReference.child("advice").child("happy").push().setValue(msg);
+                insertAdviceData();
+
                 editdt.setText(null);
+
+               // insertAdviceData();
             }
         });
+    }
+
+    private void insertAdviceData(){
+        String Advice = editdt.getText().toString();
+        String moods = spinnerMoods.getSelectedItem().toString();
+
+        HashMap<String, String> newItem = new HashMap<String, String>();
+        newItem.put("mood", moods);
+        newItem.put("msg", Advice);
+
+        databaseReference.child("advice").push().setValue(newItem);
     }
 }
