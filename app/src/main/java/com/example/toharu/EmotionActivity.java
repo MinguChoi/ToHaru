4a610c9448c9f55b283863f91b9831691fcbe216
doyeon <raisin_9@naver.com>
2021-03-18 17:51:33 +0900

    pw Edittext 누르고, 확인 버튼  click 키보드 down

--- app/src/main/java/com/example/toharu/EmotionActivity.java
+++ app/src/main/java/com/example/toharu/EmotionActivity.java
@@ -3,6 +3,8 @@ package com.example.toharu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
@@ -71,6 +73,7 @@ public class EmotionActivity extends AppCompatActivity {
 
             default:
                 selected_img = (int) v.getTag();
+
                 break;
 
         }
--- app/src/main/java/com/example/toharu/LoginActivity.java
+++ app/src/main/java/com/example/toharu/LoginActivity.java
@@ -2,20 +2,29 @@ package com.example.toharu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.toharu.API.API_Auth;
 import com.example.toharu.Utils.Utils;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
     private Button           loginBTN;
     private EditText         id, pw;
     private Button         signup_BTN;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,25 @@ public class LoginActivity extends AppCompatActivity {
         id = findViewById(R.id.id_login);
         pw = findViewById(R.id.pw_login);
         signup_BTN = findViewById(R.id.signup_BTN);
+
+
+        InputMethodManager imm =(InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+
+
+        pw.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId==EditorInfo.IME_ACTION_DONE)
+                {
+                    //실제 키보드를 내리는 이벤트
+                    imm.hideSoftInputFromWindow(pw.getWindowToken(),0);
+                }
+                return false;
+            }
+        });
+
+
+
 
         loginBTN.setOnClickListener(new View.OnClickListener() {
             @Override
