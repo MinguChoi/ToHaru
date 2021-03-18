33d0172c492b819dfa96fe5b7d14ff64249d55cd
YeStar-com <80377632+YeStar-com@users.noreply.github.com>
2021-03-18 10:58:36 +0900

    ㅇㅇㅇㅇ

--- app/src/main/java/com/example/toharu/Model/Advice.java
+++ app/src/main/java/com/example/toharu/Model/Advice.java
@@ -4,11 +4,7 @@ public class Advice {
     private String mood;
     private String advice;
 
-<<<<<<< Updated upstream
-    public Advice() { }
-=======
     public Advice(){ }
->>>>>>> Stashed changes
 
     public Advice(String mood, String advice){
         this.mood = mood;
