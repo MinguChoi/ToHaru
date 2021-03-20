d30d2380c8c287018e090e785f3b9e7337f7cb49
doyeon <raisin_9@naver.com>
2021-03-20 17:06:26 +0900

    last

--- app/src/main/java/com/example/toharu/ReadActivity.java
+++ app/src/main/java/com/example/toharu/ReadActivity.java
@@ -15,9 +15,9 @@ public class ReadActivity extends AppCompatActivity {
     //----------------------------------------------------------------------------------
     // 변수 선언
     //----------------------------------------------------------------------------------
-    private TextView    day_Write_TXT;
-    private TextView    diaryArea_Write_ETXT;
-    private ImageView   emotion_Write_IMG;
+    private TextView    day_Read_TXT;
+    private TextView    diaryArea_Read_ETXT;
+    private ImageView   emotion_Read_IMG;
     //----------------------------------------------------------------------------------
 
 
@@ -34,9 +34,9 @@ public class ReadActivity extends AppCompatActivity {
     // 초기화
     //----------------------------------------------------------------------------------
     public void init(){
-        day_Write_TXT = findViewById(R.id.day_Write_TXT);
-        diaryArea_Write_ETXT = findViewById(R.id.diaryArea_Write_ETXT);
-        emotion_Write_IMG = findViewById(R.id.emotion_Write_IMG);
+//        day_Read_TXT = findViewById(R.id.day_Read_TXT);
+        diaryArea_Read_ETXT = findViewById(R.id.diaryArea_Read_ETXT);
+        emotion_Read_IMG = findViewById(R.id.emotion_Read_IMG);
 
     }
     //----------------------------------------------------------------------------------
@@ -49,10 +49,10 @@ public class ReadActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Diary theDiary = (Diary)intent.getSerializableExtra("diary");
 
-        day_Write_TXT.setText(theDiary.getDate());
-        diaryArea_Write_ETXT.setText(theDiary.getContent());
+//        day_Read_TXT.setText(theDiary.getDate());
+        diaryArea_Read_ETXT.setText(theDiary.getContent());
         int imgId = Utils.getImageByName(theDiary.getMood(), getApplicationContext());
-        emotion_Write_IMG.setImageResource(imgId);
+        emotion_Read_IMG.setImageResource(imgId);
     }
     //----------------------------------------------------------------------------------
 
